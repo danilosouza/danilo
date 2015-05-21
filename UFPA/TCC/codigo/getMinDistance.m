c93b0b2d0d04a2f0fbcbe5fd560471aa869a4901
danilosouza <dhcsouza@gmail.com>
2015-05-20 21:35:53 -0300

    profiles
    Registro dos profiles com FullSet e com 2 Desvios padrões acima e abaixo
    da média.

--- UFPA/TCC/codigo/getMinDistance.m
+++ UFPA/TCC/codigo/getMinDistance.m
@@ -11,13 +11,13 @@ function [ dist_min ] = getMinDistance( posicao_regiao, pos_pixel, peso_geo )
 
 %Variável que guarda a distância entre o pixel desejado e os pixels da
 %região de interesse, considerando o peso
+
+
 dist(1,size(v,1)) = 0;
 for k=1:size(v,1)
     dist(1,k) = peso_geo*sqrt(((pos_pixel(1,1)- r(k,1))^2) + (((pos_pixel(1,2)- c(k,1))^2)));
 end
-
 dist_min = min(dist);
-
 
 end
 
