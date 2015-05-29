ac9591bb92a0ab6fd4b1fcecc37daeddb9d1b7a4
danilosouza <dhcsouza@gmail.com>
2015-05-28 23:29:21 -0300

    resampleMatrix
    Implementação de 2 métodos para reamostrar a matriz com as informações
    espaciais dos pixels de cada subregião.

--- UFPA/TCC/codigo/resampleMatrix.m
+++ UFPA/TCC/codigo/resampleMatrix.m
@@ -9,8 +9,12 @@ function [ matriz] = resampleMatrix( mat_posicao )
 % usando uma reamostragem de "taxa" (em %).
 [N,M] = size(mat_posicao);
 matriz(N,M) = 0;
-[~, ~, v] = find(mat_posicao);
 %taxa = 10;
+
+% ############## Método 1 ############## 
+    % Este método reamostra a matriz pegando valores que estão "fator"
+    % vezes acima e abaixo da média da FDP dos pixels em questão.
+%{    
 media = mean(v);
 desvio = sqrt(var(v));
 fator = 2;
@@ -23,8 +27,23 @@ for i=1:N
         end
     end
 end
+%}
+% ############## Método 2 ############## 
+    % Este método reamostra a matriz pegando valores escolhidos de forma
+    % aleatória dentre todos os valores disponíveis. A quantidade de
+    % valores que serão escolhidos é dada por "taxa".
+[r, c, v] = find(mat_posicao);
+taxa = 0.1;
+%temp = horzcat(r,c,v);
 
-
+for i=1:round(size(v,1)*taxa) %round(size(temp,1)*taxa) 
+    index_temp = round(rand * size(v,1));
+    if index_temp == 0
+        index_temp = index_temp + 1;
+    else
+    end
+    matriz((r(index_temp,1)),(c(index_temp,1))) = v(index_temp,1);
+end
 
 end
 
