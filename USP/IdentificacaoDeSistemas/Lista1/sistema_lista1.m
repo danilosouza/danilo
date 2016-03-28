86640fcc7cbae2c4546d96d868a629d6180359a5
danilosouza <dhcsouza@gmail.com>
2016-03-28 14:42:23 -0300

    lista 1
    texto dos itens a - e

--- USP/IdentificacaoDeSistemas/Lista1/sistema_lista1.m
+++ USP/IdentificacaoDeSistemas/Lista1/sistema_lista1.m
@@ -2,13 +2,13 @@
 %H = tf(2,[10 1],'InputDelay',3);
 %Hd = c2d(H,1);
 
-% ------------- Item d-------------- %
-% plot(y_continuo_sem_perturbacao);hold;plot(y_continuo_com_perturbacao_direta_baixa,'b');
+% % ------------- Item d-------------- %
+% plot(y_continuo_sem_perturbacao);hold;plot(y_continuo_com_perturbacao_direta_baixa,'k');
 % plot(y_continuo_com_perturbacao_1_baixa);plot(y_continuo_com_perturbacao_2_baixa);
 % legend('Y sem perturbação','Y com perturbação direta ','Y com perturbação 1','Y com perturbação 2'); grid minor;
 % title('Y afetado por perturbações geradas com variância = 0.001');
 % figure;
-% plot(y_continuo_sem_perturbacao);hold;plot(y_continuo_com_perturbacao_direta_alta,'b');
+% plot(y_continuo_sem_perturbacao);hold;plot(y_continuo_com_perturbacao_direta_alta,'k');
 % plot(y_continuo_com_perturbacao_1_alta);plot(y_continuo_com_perturbacao_2_alta);
 % legend('Y sem perturbação','Y com perturbação direta ','Y com perturbação 1','Y com perturbação 2'); grid minor;
 % title('Y afetado por perturbações geradas com variância = 0.1');
@@ -16,4 +16,4 @@
 % ------------- Item e-------------- %
 plot(y_continuo_sem_perturbacao);hold;plot(y_continuo_com_perturbacao_v1_v2);
 legend('Y sem perturbação','Y com perturbações V_{1} e V_{2} '); grid minor;
-title('Y afetado duas perturbações simultâneas não-correlacionadas com variância = exp(-6)');
+title('Y afetado por duas perturbações simultâneas não-correlacionadas com variância = exp(-6)');
